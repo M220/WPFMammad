e30fb240cf48d8936ba9756d4522ec348416be69
M220 <Mohammad2x3x@Gmail.com>
2020-12-22 11:08:27 +0330

    Add project files.

---
+++ WPFMammad/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WPFMammad
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            SetUpGame();
+        }
+
+        private void SetUpGame()
+        {
+            List<string> animalEmoji = new()
+            {
+                "🐵",
+                "🐵",
+                "🦊",
+                "🦊",
+                "🐞",
+                "🐞",
+                "🐭",
+                "🐭",
+                "🐢",
+                "🐢",
+                "🐸",
+                "🐸",
+                "🦝",
+                "🦝",
+                "🐷",
+                "🐷",
+            };
+            Random random = new();
+
+            foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
+            {
+                int index = random.Next(animalEmoji.Count);
+                string nextEmoji = animalEmoji[index];
+                textBlock.Text = nextEmoji;
+                animalEmoji.RemoveAt(index);
+            }
+        }
+    }
+}
